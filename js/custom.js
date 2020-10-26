6f5cc4e4ba0c59e46cd277aa3af8c4032c7e94bb
bsneider <54788551+bsneider@users.noreply.github.com>
2020-10-26 12:54:37 -0500

    update js for the website to have a larger text for each carousel and to auto loop through each review

--- js/custom.js
+++ js/custom.js
@@ -1,55 +1,60 @@
 
-  $(function () {
+$(function () {
     'use strict'
 
     // MENU
-    $('.navbar .nav-link').on('click',function(){
+    $('.navbar .nav-link').on('click', function () {
         $(".navbar-collapse").collapse('hide');
     });
 
-    $(window).on('scroll', function() {     
-                                
+    $(window).on('scroll', function () {
+
         /*----------------------------------------------------*/
         /*  Navigtion Menu Scroll
-        /*----------------------------------------------------*/    
-        
+        /*----------------------------------------------------*/
+
         var b = $(window).scrollTop();
-        
-        if( b > 72 ){       
+
+        if (b > 72) {
             $(".navbar").addClass("scroll");
         } else {
             $(".navbar").removeClass("scroll");
-        }               
+        }
     });
 
     // TESTIMONIALS CAROUSEL
     $('#testimonials-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
+        nav: true,
+        autoplay: true,
+        navigation: true,
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
             },
-            900:{
-                items:2,
+            900: {
+                items: 2,
             },
-            1200:{
-                items:3,
-                loop:false
+            1200: {
+                items: 2,
             }
         }
     })
 
+    $(".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+    $(".owl-next").html('<i class="fa fa-chevron-right"></i>');
+
     // SMOOTHSCROLL
-    $(function() {
-      $('.navbar .nav-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });   
-     
-  });
+    $(function () {
+        $('.navbar .nav-link').on('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+});
